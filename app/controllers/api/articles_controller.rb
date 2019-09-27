db6968bca2abd0b4e26452c479dd5c6fe1a63741
JackMarx <seriousfools@gmail.com>
2019-09-27 10:52:15 -0500

    secured api key

--- app/controllers/api/articles_controller.rb
+++ app/controllers/api/articles_controller.rb
@@ -1,7 +1,7 @@
 class Api::ArticlesController < ApplicationController
   def index
     
-    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=38fcaaa270b044c6b3b8107da778b56d")
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:search]}&apiKey=#{ ENV['API_KEY'] }")
     @articles = response.parse["articles"]
     render json: @articles
   end
